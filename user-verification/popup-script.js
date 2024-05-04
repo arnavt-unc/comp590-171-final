b3dcd2297ed772f40b9f84160fba848a6c48324e
Serenity Phillips <sphillips@unc.edu>
2024-05-04 08:52:19 -0400

    new packages for building of extension user-verification
    installed new packages for extension

--- user-verification/popup-script.js
+++
@@ -1,9 +0,0 @@
-document.querySelector('#sign-in').addEventListener('click', function () {
-    chrome.runtime.sendMessage({ message: 'login' }, function (response) {
-    });
-});
-
-document.querySelector('button').addEventListener('click', function() {
-    chrome.runtiem.sendMessage({ message: 'isUsersSignedIn' }, function(response) {
-    });
-});
