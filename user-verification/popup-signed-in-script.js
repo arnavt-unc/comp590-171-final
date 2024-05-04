52838be948af5d5c7702cb72f0cda6e3e670d1d1
Serenity Phillips <sphillips@unc.edu>
2024-05-04 08:57:54 -0400

    edits on app construction
    - finished installing new packages, starting app construction

--- user-verification/popup-signed-in-script.js
+++
@@ -1,9 +0,0 @@
-document.querySelector('#sign-out').addEventListener('click', function () {
-    chrome.runtime.sendMessage({ message: 'logout' }, function (response) {
-    });
-});
-
-document.querySelector('button').addEventListener('click', function() {
-    chrome.runtiem.sendMessage({ message: 'isUsersSignedIn' }, function(response) {
-    });
-});
